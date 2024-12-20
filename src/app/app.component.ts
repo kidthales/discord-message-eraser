d291bd98bff14791716ceb13312ddf1953698b75
kidthales <kidthales@agogpixel.com>
2024-12-19 20:18:56 -0800

    Stub app layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { invoke } from '@tauri-apps/api/core';
+
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  greetingMessage = '';
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>('greet', { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-}
+export class AppComponent {}
---
+++ src/app/core/layout/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {}
